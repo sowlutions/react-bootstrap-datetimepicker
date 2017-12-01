7805078ff08dab4cb0ad2afc74092f7570cda034
Roc Khalil <roc@rockhalil.com>
2017-12-01 11:34:19 +0200

    Fix proptypes

--- src/DateTimePickerMonths.js
+++ src/DateTimePickerMonths.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from "react";
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
 import classnames from "classnames";
 import moment from "moment";
 
@@ -53,4 +54,3 @@ export default class DateTimePickerMonths extends Component {
     );
   }
 }
-
--- src/DateTimePickerYears.js
+++ src/DateTimePickerYears.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from "react";
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
 import classnames from "classnames";
 
 export default class DateTimePickerYears extends Component {
@@ -55,4 +56,3 @@ export default class DateTimePickerYears extends Component {
     );
   }
 }
-
